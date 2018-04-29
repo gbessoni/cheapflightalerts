c5671d904546c65a8ec017ca141a2b06b9897d52
Rustam K <rustamk9192@gmail.com>
2018-04-29 12:00:18 +0300

    home changes

--- pages/index.js
+++ pages/index.js
@@ -116,6 +116,98 @@ const Index = () => (
 
     </section>
 
+    {/* Testimonials */}
+
+    <section className="section home-testimonials">
+
+      <div className="heading-secondary heading-secondary--bold text-center">
+        <i className="ion-chatboxes"/>
+        <h2>
+          Testimonials
+        </h2>
+      </div>
+
+      <div className="custom-row">
+
+        <div className="testimonial-item">
+          <div className="testimonial-item__card">
+
+            <div className="testimonial-item__text">
+              How could I not book this Business class flight to Barcelona for $542 roundtrip?? Saved me over $1,500 on airfare! Absolutely an amazing deal, I did it, and I'm super excited. Your premium newsletter awesome
+            </div>
+
+            <div className="testimonial-item__author">
+              {/* <span className="testimonial-item__author__icon">&#128630;</span> */}
+              <span className="testimonial-item__author__icon">
+                <i className="ion-quote" />
+              </span>
+              <span className="testimonial-item__author__main">- Jonathan F, April 2018</span>
+              <span className="testimonial-item__author__sub">Saved $1500 on flights</span>
+            </div>
+
+          </div>
+        </div>
+
+        <div className="testimonial-item">
+          <div className="testimonial-item__card">
+
+            <div className="testimonial-item__text">
+              I can't thank you enough! I have been watching fares to Sydney like a hawk for nearly 25 years and this is the absolute lowest I've even seen them. It's good to know that when you send fare alerts, they're the real deal!
+            </div>
+
+            <div className="testimonial-item__author">
+              <span className="testimonial-item__author__icon">
+                <i className="ion-quote" />
+              </span>
+              <span className="testimonial-item__author__main">- Jackie L, March 2018</span>
+              <span className="testimonial-item__author__sub">Saved $1200 on flights</span>
+            </div>
+
+          </div>
+        </div>
+
+        <div className="testimonial-item">
+          <div className="testimonial-item__card">
+
+            <div className="testimonial-item__text">
+              We booked a trip to Dublin, Ireland for later this year on a Dollar Flight Club fare alert :) I saved about $1,500 on four tickets for the family. We're so excited. Thanks so much for a great service and doing what your team does!
+            </div>
+
+            <div className="testimonial-item__author">
+              <span className="testimonial-item__author__icon">
+                <i className="ion-quote" />
+              </span>
+              <span className="testimonial-item__author__main">- Steve M, January 2018</span>
+              <span className="testimonial-item__author__sub">Saved $1,500+ on flights</span>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+
+      </section>
+
+    {/* About */}
+
+    <section className="section home-about">
+
+      <div className="heading-secondary heading-secondary--bold text-center">
+        <h2>
+          About us
+        </h2>
+      </div>
+
+      <p className="home-about__text">
+        Cheap Flight Alerts is your only source for cheap flight deals, because we source our deals from all the greatest websites, blogs, newsletters and twitter feeds, to deliver the latest and lowest airfares, both Domestic and International. And since our service allows you to customize when and where you want to fly, you receive relevant deals 100% of the time. Try us out today!
+      </p>
+
+      <p className="home-about__text">
+        Still have Questions - Check out our FAQs and Help section <a href="http://cheapflights.helpsite.io/" className="link">Click here</a>
+      </p>
+
+      </section>
+
     {/* <SubscribeModal /> */}
 
     <SubscribeModal />
