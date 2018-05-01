18a2890902fb6241df8580983fae1511e4517ce0
Rustam K <rustamk9192@gmail.com>
2018-05-01 13:03:18 +0300

    dev mode

--- config.js
+++ config.js
@@ -1,17 +1,21 @@
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
 
-  console.log(process.env.NODE_ENV);
+//   console.log(process.env.NODE_ENV);
 
-  module.exports = {
-    API: 'http://api.cheapflightalerts.net',
-  };
+//   module.exports = {
+//     API: 'http://api.cheapflightalerts.net',
+//   };
 
-} else {
+// } else {
 
-  console.log(process.env.NODE_ENV);
+//   console.log(process.env.NODE_ENV);
 
-  module.exports = {
-    API: 'http://staging.api.cheapflightalerts.net',
-  };
+//   module.exports = {
+//     API: 'http://staging.api.cheapflightalerts.net',
+//   };
 
-}
+// }
+
+module.exports = {
+  API: 'http://staging.api.cheapflightalerts.net',
+};
