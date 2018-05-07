8bf54755ff0373b0a8662c184e61b99e2e78d4e6
Rustam9192 <rustamk9192@gmail.com>
2018-05-07 14:45:07 +0300

    prod url fixed to https

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 if (process.env.NODE_ENV === 'production') {
 
-  console.log(process.env.NODE_ENV);
+    console.log(process.env.NODE_ENV);
 
-  module.exports = {
-    API: 'http://api.cheapflightalerts.net',
-  };
+    module.exports = {
+        API: 'https://api.cheapflightalerts.net',
+    };
 
 } else {
 
-  console.log(process.env.NODE_ENV);
+    console.log(process.env.NODE_ENV);
 
-  module.exports = {
-    API: 'http://staging.api.cheapflightalerts.net',
-  };
+    module.exports = {
+        API: 'http://staging.api.cheapflightalerts.net',
+    };
 
 }
