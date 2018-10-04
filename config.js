8b9da06972182cbb672eda45fd71f47f399c5dfd
Rustam K <rustamk9192@gmail.com>
2018-10-04 16:17:51 +0300

    staging setup

--- config.js
+++ config.js
@@ -1,17 +1,21 @@
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
+//
+//     console.log(process.env.NODE_ENV);
+//
+//     module.exports = {
+//         API: 'https://api.cheapflightalerts.net',
+//     };
+//
+// } else {
+//
+//     console.log(process.env.NODE_ENV);
+//
+//     module.exports = {
+//         API: 'http://staging.api.cheapflightalerts.net',
+//     };
+//
+// }
 
-    console.log(process.env.NODE_ENV);
-
-    module.exports = {
-        API: 'https://api.cheapflightalerts.net',
-    };
-
-} else {
-
-    console.log(process.env.NODE_ENV);
-
-    module.exports = {
-        API: 'http://staging.api.cheapflightalerts.net',
-    };
-
-}
+module.exports = {
+    API: 'http://staging.api.cheapflightalerts.net',
+};
